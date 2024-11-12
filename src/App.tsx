40b56ada0f0df1257258f52721591000bcc3b221
Tirlochan Arora <tirlochan.arora@seekout.com>
2024-11-12 15:14:00 +0530

    minor update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
+import { useEffect, useState } from "react";
 import SeekoutLogo from "./components/SeekoutLogo";
 import SelectRole from "./components/SelectRole";
 import SelectTemplate from "./components/SelectTemplate";
 import { useAppContext } from "./context/AppContext";
 
 function App() {
+  const [userType, setUserType] = useState<
+    "recruiter" | "candidate" | null
+  >(null);
+
   const { currentScreen } = useAppContext();
 
-  const getRenderedScreen = () => {
+  const getRecruiterScreens = () => {
     switch (currentScreen) {
       case "SelectRole":
         return <SelectRole />;
       case "SelectTemplate":
-        return <SelectTemplate />
+        return <SelectTemplate />;
       default:
         return null;
     }
-  }
+  };
+
+  const getCandidateScreens = () => {
+    return null
+  };
+
+  useEffect(() => {
+    const userType = document.location.search.split("=")[1].toLowerCase();
+    setUserType(userType as "recruiter" | "candidate");
+  }, []);
+
+  const screens = userType === "recruiter" ? getRecruiterScreens : getCandidateScreens;
+
   return (
     <div>
       <SeekoutLogo />
-      {getRenderedScreen()}
+      {userType && screens()}
     </div>
   );
 }
