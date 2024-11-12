ae3158432dcbf349f9ac6c5654f173b0df7055bb
Tirlochan Arora <tirlochan.arora@seekout.com>
2024-11-12 12:51:22 +0530

    minor update

--- src/components/SelectRole/index.tsx
+++ src/components/SelectRole/index.tsx
@@ -32,20 +32,22 @@ const SelectRole = () => {
       <h2 className="font-semibold text-3xl text-center mx-6 leading-10">
         Which role are you hiring for?
       </h2>
-      <div className="mt-10 w-full px-4 flex flex-col items-center gap-5">
-        {ROLES_LIST.map((role) => {
-          return (
-            <CustomRadio
-              id={role.id}
-              checked={selectedRole === role.id}
-              name="role"
-              value={role.id}
-              onChange={handleRoleChange}
-              label={role.name}
-              key={role.id}
-            />
-          );
-        })}
+      <div className="mt-10 w-full px-4">
+        <form className="w-full flex flex-col items-center gap-5">
+          {ROLES_LIST.map((role) => {
+            return (
+              <CustomRadio
+                id={role.id}
+                checked={selectedRole === role.id}
+                name="role"
+                value={role.id}
+                onChange={handleRoleChange}
+                label={role.name}
+                key={role.id}
+              />
+            );
+          })}
+        </form>
       </div>
       <div className="mt-10 w-full px-4">
         <Button
